641a3932abeff098f84a48e937975fd774320b37
Light <511272827@qq.com>
2023-12-04 12:59:00 +0800

    Add LOG_FILE_LINE for easily debugging

--- inc/mcc/log.h
+++ inc/mcc/log.h
@@ -7,6 +7,10 @@
 #define NO_DEBUG 0
 #endif
 
+#ifndef NO_LOG_FILE_LINE
+#define NO_LOG_FILE_LINE 0
+#endif
+
 #define LOG_LEVEL_DEBUG 0
 #define LOG_LEVEL_INFO  1
 #define LOG_LEVEL_WARN  2
@@ -15,12 +19,17 @@
 
 #define log_print printf
 
-#define LOG_DEBUG(...)  do { if (!NO_DEBUG && LOG_LEVEL <= LOG_LEVEL_DEBUG)  log_print(__VA_ARGS__); } while(0)
+#define LOG_FILE_LINE log_print("[%s:%d]", __FILE__, __LINE__)
+
+#define LOG_HERE  do { log_print("[%s:%d]\n", __FILE__, __LINE__); } while(0)
+#define LOG_HERE_ARGS(fmt, ...) do { log_print("[%s:%d] " fmt, __FILE__, __LINE__, ##__VA_ARGS__); } while(0)
+
+#define LOG_DEBUG(...)  do { if (!NO_DEBUG && LOG_LEVEL <= LOG_LEVEL_DEBUG)  { if (!NO_LOG_FILE_LINE) LOG_FILE_LINE; log_print(__VA_ARGS__); } } while(0)
 #define LOG_HERE_DEBUG  do { if (!NO_DEBUG && LOG_LEVEL <= LOG_LEVEL_DEBUG) log_print("[%s:%d]\n", __FILE__, __LINE__); } while(0)
-#define LOG_INFO(...)   do { if (LOG_LEVEL <= LOG_LEVEL_INFO)  log_print(__VA_ARGS__); } while(0)
-#define LOG_WARN(...)   do { if (LOG_LEVEL <= LOG_LEVEL_WARN)  log_print(__VA_ARGS__); } while(0)
-#define LOG_ERROR(...)  do { if (LOG_LEVEL <= LOG_LEVEL_ERROR)  log_print(__VA_ARGS__); } while(0)
-#define LOG_FATAL(...)  do { if (LOG_LEVEL <= LOG_LEVEL_FATAL)  log_print(__VA_ARGS__); abort(); } while(0)
+#define LOG_INFO(...)   do { if (LOG_LEVEL <= LOG_LEVEL_INFO)  { if (!NO_LOG_FILE_LINE) LOG_FILE_LINE; log_print(__VA_ARGS__); } } while(0)
+#define LOG_WARN(...)   do { if (LOG_LEVEL <= LOG_LEVEL_WARN)  { if (!NO_LOG_FILE_LINE) LOG_FILE_LINE; log_print(__VA_ARGS__); } } while(0)
+#define LOG_ERROR(...)  do { if (LOG_LEVEL <= LOG_LEVEL_ERROR) { if (!NO_LOG_FILE_LINE) LOG_FILE_LINE; log_print(__VA_ARGS__); } } while(0)
+#define LOG_FATAL(...)  do { if (LOG_LEVEL <= LOG_LEVEL_FATAL) { if (!NO_LOG_FILE_LINE) LOG_FILE_LINE; log_print(__VA_ARGS__); abort(); } } while(0)
 
 #define LOG_LEVEL   LOG_LEVEL_DEBUG
 
@@ -32,9 +41,6 @@
 
 #define error err
 #define always log_print
-
-#define LOG_HERE  do { log_print("[%s:%d]\n", __FILE__, __LINE__); } while(0)
-#define LOG_HERE_ARGS(fmt, ...) do { log_print("[%s:%d] " fmt, __FILE__, __LINE__, ##__VA_ARGS__); } while(0)
 
 #define LH LOG_HERE
 #define LHD LOG_HERE_DEBUG
