afaf87ca4c5c85aae16c8462f8f8b661daf3c7bf
Light <511272827@qq.com>
2023-12-09 10:37:41 +0800

    Update script for pull and push from gitee or github

--- p.bat
+++ p.bat
@@ -1,1 +1,15 @@
-git pull origin master
+@echo off
+
+:: ** Please config locally for the file **
+
+:: Pull from gitee or github as your configuration
+:: You must config correctly for your local git.
+
+if "%1" == "gitee" (
+    git pull origin master
+) else if "%1" == "github" (
+    git pull github master
+) else (
+    :: Even use a strange parameter, we'd prefer to believe he wants to push to server, just push!
+    git pull origin master
+)
--- push.bat
+++ push.bat
@@ -1,1 +1,15 @@
-git push origin master
+@echo off
+
+:: ** Please config locally for the file **
+
+:: Push to gitee or github as your configuration
+:: You must config correctly for your local git.
+
+if "%1" == "gitee" (
+    git push origin master
+) else if "%1" == "github" (
+    git push github master
+) else (
+    :: Even use a strange parameter, we'd prefer to believe he wants to push to server, just push!
+    git push origin master
+)
