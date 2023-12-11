09e1f75f6a4fd070d0fba1bffd63c40c2a34cfe8
Light <511272827@qq.com>
2023-12-11 21:52:06 +0800

    Add allocm macro to allocate memory buffer easily

--- inc/mcc/mem.h
+++ inc/mcc/mem.h
@@ -4,16 +4,16 @@
 #include "mcc/features.h"
 
 #ifndef __cplusplus
-#define mcc_malloc  malloc
-#define mcc_free	free
+#define mcc_malloc malloc
+#define mcc_free free
 
-#define mcc_calloc  calloc
+#define mcc_calloc calloc
 #define mcc_realloc realloc
 #else
-#define mcc_malloc  cpp_malloc
-#define mcc_free	cpp_free
+#define mcc_malloc cpp_malloc
+#define mcc_free cpp_free
 
-#define mcc_calloc  cpp_calloc
+#define mcc_calloc cpp_calloc
 #define mcc_realloc cpp_realloc
 
 void *cpp_malloc(size_t size);
@@ -21,5 +21,9 @@ void cpp_free(void *p);
 void *cpp_calloc(size_t num, size_t size);
 void *cpp_realloc(void *p, size_t size);
 #endif
+
+// All threads will share the same memory buffer.
+// It's important to make them synchronous.
+#define allocm(size) alloc_from_mem_buf(&GlobalMemBuf, size)
 
 #endif
