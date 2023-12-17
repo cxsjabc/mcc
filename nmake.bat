7dfd49a9ce74312b0bada2cb19bf7d3e0c1740c6
Light <511272827@qq.com>
2023-12-18 07:56:14 +0800

    Fix nmake multi execute problem: vs2019 env re-init issue

--- nmake.bat
+++ nmake.bat
@@ -7,12 +7,22 @@ set MSVC_ENV_BAT="D:\ProgramFiles\MicrosoftVisualStudio\2019\Community\VC\Auxili
 :: x64 or x86, default x64
 set MSVC_ARCH=x64
 
-if defined __VCVARSALL_HOST_ARCH (
+set OUT_DIR=out
+set PREVIOUS_BUILD=win_arch.txt
+
+mkdir %OUT_DIR%
+:: TODO: strange that, it may be executed three times, then VSCMD_VCVARSALL_INIT is ok.
+rem echo "VSCMD_VCVARSALL_INIT: %VSCMD_VCVARSALL_INIT%"
+
+cl > nul 2>&1
+
+if %errorlevel% equ 0 (
     echo VCVARS*.bat is executed, ignore
 ) else (
+:need-init
     echo VCVARS*.bat is not executed
-    :: TODO: strange that, it may be executed twice or more.
     call %MSVC_ENV_BAT% %MSVC_ARCH%
+    echo %MSVC_ARCH% > %OUT_DIR%\%PREVIOUS_BUILD%
 )
 
 set WIN_GEN_SRC=scripts\windows\win_gen_src.bat
@@ -31,10 +41,12 @@ if "%1" == "" (
     nmake.exe -f makefiles\Makefile.msvc.mk mcc
 ) else if "%1" == "clean" (
     nmake.exe -f makefiles\Makefile.msvc.mk clean
+    del %OUT_DIR%\%PREVIOUS_BUILD%
 )  else if "%1" == "test" (
     call %WIN_GEN_SRC%
 
     nmake.exe -f makefiles\Makefile.msvc.mk test
 ) else if "%1" == "clean_test" (
     nmake.exe -f makefiles\Makefile.msvc.mk clean_test
+    del %OUT_DIR%\%PREVIOUS_BUILD%
 )
