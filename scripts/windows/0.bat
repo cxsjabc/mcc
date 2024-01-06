9ae982e2e09817cdde75b78ea0696ec366d9807d
Light <511272827@qq.com>
2024-01-06 19:50:22 +0800

    Remove more temp files

--- scripts/windows/0.bat
+++ scripts/windows/0.bat
@@ -5,3 +5,6 @@ rm -rf out
 rm mcc mcc_test
 rm mcc.exe mcc_test.exe
 rm mcc.ilk mcc.pdb mcc_test.ilk mcc_test.pdb mcc_test.exe.stackdump
+rm a.exe
+rm m t m.exe t.exe
+rm m.exe.stackdump t.exe.stackdump
