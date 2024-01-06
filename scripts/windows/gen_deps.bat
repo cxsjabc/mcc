d451d354e81cf6db4663382e82b1f350828e8671
Light <511272827@qq.com>
2024-01-06 09:36:37 +0800

    Remove unused debug logs

--- scripts/windows/gen_deps.bat
+++ scripts/windows/gen_deps.bat
@@ -14,17 +14,15 @@ if "%1" neq "" (
     )
 
     set DEPS=%BUILD_OUT_FOLDER%\%1\%1.deps.win
-    echo "DEPS: "!DEPS!
+    rem echo "DEPS: "!DEPS!
     
     echo.> !DEPS!
     for %%i in (%1/*.c) do (
         set OBJ=%1\%%~ni%OBJ_POSTFIX%
-        echo "OBJ: "!OBJ!
+        rem echo "OBJ: "!OBJ!
 
         echo %BUILD_OUT_FOLDER%\!OBJ!: \>> !DEPS!
-        echo xxx
         cl.exe -I ./inc /c /showIncludes %1\%%~ni.c /nologo /Fo:NUL | findstr -v "No such file" | findstr "inc\mcc" | %AWK% "{print $3}" | sed '1,$s#$# \\\#' >> !DEPS!
-        echo yy
         echo.>> !DEPS!
         echo.>> !DEPS!
     )
