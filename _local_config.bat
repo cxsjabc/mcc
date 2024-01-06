5a103adcdaa5d5bdcf957f9fd58b19a760ea16ca
Light <511272827@qq.com>
2024-01-06 09:33:27 +0800

    Add _local_config_example.bat

--- _local_config.bat
+++
@@ -1,2 +0,0 @@
-
-set MSVC_ENV_BAT="F:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat"
---
+++ _local_config_example.bat
@@ -0,0 +1,2 @@
+
+set MSVC_ENV_BAT="D:\ProgramFiles\MicrosoftVisualStudio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"
