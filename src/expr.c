9560b1c76e8a7dd249d22515f46a4fe4e0b048b0
Light <511272827@qq.com>
2024-02-02 23:11:05 +0800

    Add more expr

--- src/expr.c
+++ src/expr.c
@@ -13,42 +13,89 @@
 
 __BEGIN_DECLS
 
-void assignment_expression()
+void expr()
 {
-	equality_expression();
-	if (Tok->type == TOK_ASSIGN) 
+	assignment_expr();
+	while (Tok->type == TOK_COMMA) 
 	{
-		check_lvalue();
 		NEXT;
-		assignment_expression();
+		assignment_expr();
 		store0_1();
 	}
 }
 
-void equality_expression()
+void assignment_expr()
+{
+	cond_expr();
+	if (Tok->type == TOK_ASSIGN) 
+	{
+		check_lvalue();
+		NEXT;
+		assignment_expr();
+		store0_1();
+	}
+}
+
+void constant_expr()
+{
+	cond_expr();
+}
+
+void cond_expr()
+{
+	logic_or_expr();
+	if (Tok->type == TOK_QUESTION) {
+		expr();
+		if (Tok->type == TOK_COLON) {
+			NEXT;
+			cond_expr();
+		} else
+			expect(":");
+	}
+}
+
+void logic_or_expr()
+{
+	logic_and_expr();
+	if (Tok->type == TOK_LOR) {
+		NEXT;
+		logic_and_expr();
+	}
+}
+
+void logic_and_expr()
+{
+	or_expr();
+	if (Tok->type == TOK_LAND) {
+		NEXT;
+		or_expr();
+	}
+}
+
+void equality_expr()
 {
 	Token t;
 
-	relational_expression();
+	relational_expr();
 	while (Tok->type == TOK_EQUAL || Tok->type == TOK_NEQ) 
 	{
 		t = Tok;
 		NEXT;
-		relational_expression();
+		relational_expr();
 		gen_op(t);
 	}
 }
 
-void relational_expression()
+void relational_expr()
 {
 	int t;
-	additive_expression();
+	additive_expr();
 	while ((token == TK_LT || token == TK_LEQ) ||
 		token == TK_GT || token == TK_GEQ) 
 	{
 		t = token;
 		get_token();
-		additive_expression();
+		additive_expr();
 		gen_op(t);
 	}
 }
