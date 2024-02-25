83e5dc081b9125a31c2cb58733fdb85e6ca639be
Light <511272827@qq.com>
2024-02-25 09:44:02 +0800

    Update section header

--- inc/mcc/win/section_header.h
+++ inc/mcc/win/section_header.h
@@ -1,5 +1,13 @@
-/* Copied from: C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\um\winnt.h */
+#ifndef SECTION_HEADER_H
+#define SECTION_HEADER_H
 
+#include "mcc/mcc_base.h"
+
+#include <winnt.h>
+__BEGIN_DECLS
+
+#ifdef NO_WINNT
+/* Copied from: C:\Program Files (x86)\Windows Kits\10\Include\10.0.17763.0\um\winnt.h */
 typedef struct _IMAGE_SECTION_HEADER {
     BYTE    Name[IMAGE_SIZEOF_SHORT_NAME];
     union {
@@ -15,3 +23,15 @@ typedef struct _IMAGE_SECTION_HEADER {
     WORD    NumberOfLinenumbers;
     DWORD   Characteristics;
 } IMAGE_SECTION_HEADER, *PIMAGE_SECTION_HEADER;
+#endif
+
+typedef struct section
+{
+	int offset;
+	unsigned char *data;
+	unsigned int size;
+
+	IMAGE_SECTION_HEADER hdr;
+} *Section;
+
+#endif
